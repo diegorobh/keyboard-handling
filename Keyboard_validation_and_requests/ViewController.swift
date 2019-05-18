3dc365833b7ace0934d86b6de5bf646dd928c4bf
Wilman Rojas <wrojas@>
2019-05-18 10:56:09 -0500

    Initial Commit

---
+++ Keyboard_validation_and_requests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Keyboard_validation_and_requests
+//
+//  Created by Diego Paredes on 5/18/19.
+//  Copyright © 2019 D-Fine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Keyboard_validation_and_requestsUITests/Keyboard_validation_and_requestsUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Keyboard_validation_and_requestsUITests.swift
+//  Keyboard_validation_and_requestsUITests
+//
+//  Created by Diego Paredes on 5/18/19.
+//  Copyright © 2019 D-Fine. All rights reserved.
+//
+
+import XCTest
+
+class Keyboard_validation_and_requestsUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
